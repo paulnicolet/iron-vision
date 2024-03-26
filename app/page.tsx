1862e3b95c876c8befd3a57f605a3734349040a0
Paul Nicolet <nicoletpaul@orange.fr>
2024-03-26 17:15:14 +0100

    Add image and first name

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 import { unauthenticate } from './action';
-import { isLoggedIn } from './lib/garmin';
+import { getClient, isLoggedIn } from './lib/garmin';
 
 export default async function Home() {
   if (!isLoggedIn()) {
     return redirect('/login');
   }
 
+  const profile = await getClient().getUserProfile();
+  var firstname = 'you';
+  try {
+    firstname = profile.fullName.split(' ')[0];
+  } catch {}
+
   return (
     <main>
-      <h1>Hello, press ⌘K</h1>
+      <Image
+        width="40"
+        height="40"
+        src={profile.profileImageUrlSmall}
+        alt="profile picture"
+        style={{ marginTop: 20 }}
+      />
+      <h1>Hello {firstname}, press ⌘K</h1>
       <form action={unauthenticate}>
         <button type="submit">Logout</button>
       </form>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        port: '',
+        pathname: '/garmin-connect-prod/profile_images/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
